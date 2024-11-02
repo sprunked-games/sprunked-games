11893dfc4e2d7dc7e346786ae2d5b9e06714801a
myncepu <myncepu@gmail.com>
2024-11-02 18:27:46 +0800

    Update layout to include PlausibleProvider; add next-plausible

--- apps/web/app/[locale]/(public)/layout.tsx
+++ apps/web/app/[locale]/(public)/layout.tsx
@@ -1,44 +1,48 @@
-import { NextUIProvider } from "@nextui-org/system"
-import { defaultLocale } from "@repo/i18n/locales"
-import "@repo/tailwind-config/globals.css"
-import { NextIntlClientProvider } from "next-intl"
-import { getMessages } from "next-intl/server"
-import type { ReactNode } from "react"
+import { NextUIProvider } from "@nextui-org/system";
+import { defaultLocale } from "@repo/i18n/locales";
+import "@repo/tailwind-config/globals.css";
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+import type { ReactNode } from "react";
+import PlausibleProvider from 'next-plausible'
 
-import { Toaster } from "@repo/ui/toaster"
-import { Footer, MicrosoftClarity, Navbar } from "@repo/web/lib/components"
-import { siteConfig } from "@repo/web/lib/config/site"
-import { GoogleAnalytics } from "@next/third-parties/google"
+
+import { Toaster } from "@repo/ui/toaster";
+import { Footer, MicrosoftClarity, Navbar } from "@repo/web/lib/components";
+import { siteConfig } from "@repo/web/lib/config/site";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 type Props = {
-    children: ReactNode
-    params: { locale: string }
-}
+  children: ReactNode;
+  params: { locale: string };
+};
 
 export default async function LocaleLayout({
-    children,
-    params: { locale = defaultLocale }
+  children,
+  params: { locale = defaultLocale },
 }: Props) {
-    const isDev = process.env.NODE_ENV === "development"
-    const messages = await getMessages()
-    return (
-        
-        <NextIntlClientProvider messages={messages} locale={locale}>
-  
-        <NextUIProvider>
-            <Toaster />
-            <Navbar />
-            {children}
-            <Footer />
-            {
-                !isDev && (
-                    <>
-                        <GoogleAnalytics gaId={siteConfig.gaId as string}/>
-                        <script defer data-domain={siteConfig.domain} src="https://app.pageview.app/js/script.js"></script>
-                    </>
-                )
-            }
-        </NextUIProvider>
+  const isDev = process.env.NODE_ENV === "development";
+  const messages = await getMessages();
+  return (
+    <NextIntlClientProvider messages={messages} locale={locale}>
+        <PlausibleProvider domain={siteConfig.domain}>
+            <NextUIProvider>
+                <Toaster />
+                <Navbar />
+                {children}
+                <Footer />
+                {!isDev && (
+          <>
+            <GoogleAnalytics gaId={siteConfig.gaId as string} />
+            <script
+              defer
+              data-domain={siteConfig.domain}
+              src="https://app.pageview.app/js/script.js"
+            ></script>
+          </>
+        )}
+      </NextUIProvider>
+      </PlausibleProvider>
     </NextIntlClientProvider>
-    )
+  );
 }
